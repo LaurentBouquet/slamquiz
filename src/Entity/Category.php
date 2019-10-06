f5698b54954a59a9134c5ff126edb323c5601ef4
Laurent Bouquet <laurent.bouquet@joliciel.fr>
2019-10-06 17:43:30 +0200

    [Core] Category entity

---
+++ src/Entity/Category.php
@@ -0,0 +1,63 @@
+<?php
+
+use Doctrine\ORM\Mapping\Table;
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="tbl_category")
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @var int The id of this category
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string The shortname of the category
+     * @ORM\Column(type="string", length=50)
+     */
+    private $shortname;
+
+    /**
+     * @var string The longname of the category
+     * @ORM\Column(type="string", length=255)
+     */
+    private $longname;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getShortname(): ?string
+    {
+        return $this->shortname;
+    }
+
+    public function setShortname(string $shortname): self
+    {
+        $this->shortname = $shortname;
+
+        return $this;
+    }
+
+    public function getLongname(): ?string
+    {
+        return $this->longname;
+    }
+
+    public function setLongname(string $longname): self
+    {
+        $this->longname = $longname;
+
+        return $this;
+    }
+}
